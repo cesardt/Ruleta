e41df42b344ca7f528a2bd214ebbfa62e8efb16f
Edna Ramirez <edna.jahel.r@gmail.com>
2013-11-12 12:53:14 -0600

    Wheel & NonRandom

--- RuletaV2/src/model/game/com/NonRandom.java
+++ RuletaV2/src/model/game/com/NonRandom.java
@@ -1,17 +1,17 @@
 package model.game.com;
 
 public class NonRandom extends java.util.Random {
-	
+	private int value;
 	public NonRandom(){
 		
 	}
 	
-	protected int next(int bis){
-		
+	protected int next(int bits){
+		return value;
 	}
 	
 	public void setSeed(long seed){
-		
+		this.value = (int) seed;
 	}
 
 }
--- RuletaV2/src/model/game/com/Wheel.java
+++ RuletaV2/src/model/game/com/Wheel.java
@@ -1,13 +1,13 @@
 package model.game.com;
 
 public class Wheel {
-	
+	private java.util.Vector<Bin> bins = new java.util.Vector<Bin>(38);
+	private java.util.Random rng;
 	public Wheel(){
-		
 	}
 	
 	public Wheel(java.util.Random rng){
-		
+		this.rng=rng;
 	}
 	
 	public void addBin(int binPosition, Bin bin){
@@ -15,11 +15,11 @@ public class Wheel {
 	}
 	
 	public void addOutcome(int bin, Outcome outcome){
-		
+		bins.get(bin).add(outcome);
 	}
 	
 	public Bin getBin(int bin){
-		
+		return this.bins.get(bin);
 	}
 	
 	public Outcome getOutcome(String name){
@@ -27,11 +27,11 @@ public class Wheel {
 	}
 	
 	public java.util.Random getRng(){
-		
+		return rng;
 	}
 	
 	public Bin next(){
-		
+		return bins.get(rng.nextInt(38));
 	}
 
 }
