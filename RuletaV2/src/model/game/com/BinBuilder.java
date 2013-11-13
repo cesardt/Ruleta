1471242821caf43488c75476ee45a272bd87457e
Cesar De la Torre <cesardelatorre3@gmail.com>
2013-11-13 00:50:51 -0600

    Push BinBuilder

--- RuletaV2/src/model/game/com/BinBuilder.java
+++ RuletaV2/src/model/game/com/BinBuilder.java
@@ -37,11 +37,84 @@ public class BinBuilder {
 				wheel.getBin(i).add(new Outcome("Black",1));
 			}
 		}
+		
+		for(int i = 0; i<12; i++){ 
+			int n=3*i+1; //Odd
+			//Left-right pairs
+			Outcome split=new Outcome(n+","+(n+1)+"",17);
+			wheel.getBin(n).add(split);
+			wheel.getBin(n+1).add(split);
+			//Up-down pairs
+			split=new Outcome(n+","+(n+3)+"",17);
+			wheel.getBin(n).add(split);
+			wheel.getBin(n+3).add(split);
+			//Street pairs
+			split=new Outcome(n+","+(n+1)+","+(n+2),11);
+			wheel.getBin(n).add(split);
+			wheel.getBin(n+1).add(split);
+			wheel.getBin(n+2).add(split);
+			
+			
+			n=3*i+2; //Even
+			//Left right pairs
+			split=new Outcome(n+","+n+1+"",17);
+			wheel.getBin(n).add(split);
+			wheel.getBin(n+1).add(split);
+			//Up down pairs
+			split=new Outcome(n+","+n+3+"",17);
+			wheel.getBin(n).add(split);
+			wheel.getBin(n+3).add(split);
+			//Street pairs
+			split=new Outcome(n+","+(n+1)+","+(n+2),11);
+			wheel.getBin(n).add(split);
+			wheel.getBin(n+1).add(split);
+			wheel.getBin(n+2).add(split);
+			
+			
+			
+		}
+		
+		for(int i=0;i<11;i++){
+			int n=3*i+1;
+			Outcome street=new Outcome(n+","+(n+1)+","+(n+3)+","+(n+4),8);
+			wheel.getBin(n).add(street);
+			wheel.getBin(n+1).add(street);
+			wheel.getBin(n+3).add(street);
+			wheel.getBin(n+4).add(street);
+			
+			n=3*i+2;
+			street=new Outcome(n+","+(n+1)+","+(n+3)+","+(n+4),8);
+			wheel.getBin(n).add(street);
+			wheel.getBin(n+1).add(street);
+			wheel.getBin(n+3).add(street);
+			wheel.getBin(n+4).add(street);
+		}
+		for(int i=0;i<10;i++){
+			int n=i*3+1;
+			Outcome line=new Outcome(n+","+(n+1)+","+(n+2)+","+(n+3)+","+(n+4)+","+(n+5),5);
+			wheel.getBin(n).add(line);
+			wheel.getBin(n+1).add(line);
+			wheel.getBin(n+2).add(line);
+			wheel.getBin(n+3).add(line);
+			wheel.getBin(n+4).add(line);
+			wheel.getBin(n+5).add(line);
+		}
+		for(int i=0;i<3;i++){
+			Outcome column=new Outcome(String.valueOf(i+1),2);
+			for(int j=0;j<12;j++){
+				wheel.getBin(3*j+i+1).add(column);
+			}
+		}
+		
 		temp = new Bin();
 		temp.add(new Outcome("00",35));
 		wheel.addBin(37, temp);
 	}
 	
-	
+	public static void main(String [] args){
+		BinBuilder bb = new BinBuilder();
+		Wheel wheel = new Wheel();a	Z
+		bb.buildBins(wheel);
+	}
 
 }
